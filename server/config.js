252181e7fcbdaa2ded17f9f0c45432f8aa13b2ac
Jascha Ehrenreich <jascha@jaeh.at>
2015-01-07 00:13:29 +0100

    changed internal port to 5000

--- server/config.js
+++ server/config.js
@@ -5,15 +5,15 @@ module.exports = {
   defaults: {
     development: {
       host: "http://localhost:80"
-    , PORT: 80
+    , PORT: 5000
     }
   , production: {
       host: "https://production.com"
-    , PORT: 80
+    , PORT: 5000
     }
   , staging: {
       host: "https://staging.production.com"
-    , PORT: 80
+    , PORT: 5000
     }
   }
   , mail: 'mail@mail.mail'
